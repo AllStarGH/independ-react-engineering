58bbeb26873dc17f83a260bf9fa8f5c1b46fd43c
AllStarGH <duernna@163.com>
2020-08-31 01:08:00 +0800

    add The user controller base on typescript.

--- background-server/src/app.module.ts
+++ background-server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserController } from './com/allstar/glories/controller/user/user.controller';
+
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ background-server/src/com/allstar/glories/controller/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+	@Get()
+	index():string{
+		return '这里是user模块的index';
+	}
+
+	@Get('interviewHandler')
+	interviewHandler():object{
+		let gentleman = ({ name: 'victoira', salaries: 2300, gender: 'male', age: 23 ,phoneNum:'16602573234'});
+		return gentleman;
+	}
+}
