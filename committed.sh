69d98f2a545bd4c4de40852c5f2cdaa7da06fcd2
AllStarGH <duernna@163.com>
2020-09-03 01:26:32 +0800

    test the english controller.

--- committed.sh
+++ committed.sh
@@ -7,7 +7,7 @@
 
 
 git add .
-git commit -m 'test the ennglish controller.'
+git commit -m 'test the english controller.'
 
 # error: 无法推送一些引用到 'https://github.com/AllStarGH/independ-react-engineering.git'
 # 提示：更新被拒绝，因为您当前分支的最新提交落后于其对应的远程分支。
